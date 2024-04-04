457c5dcf2ef9b6d89b1ab0f554a2b8a178885dcd
Triana Laurino <trii.laurino@gmail.com>
2024-04-04 17:06:04 +0200

    added model Cakes
    Added  file for model cakes

--- src/main/java/com/model/Cakes.java
+++ src/main/java/com/model/Cakes.java
@@ -1,5 +1,10 @@
 package com.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQuery;
+
+@Entity
+@NamedQuery( name = "Cakes.findAll", query = "SELECT c FROM Cakes c")
 public class Cakes {
 
 }
